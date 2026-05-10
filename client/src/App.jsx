a2e05db1e7badf51a0d5a5dab78d9d19e4ad6c4d
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2026-05-10 14:21:22 +0530

    feat: initialize client project with React, Vite, and core whiteboard layout components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Canvas from "./features/whiteboard/components/Canvas";
+import Toolbar from "./features/whiteboard/components/Toolbar";
+import Sidebar from "./features/whiteboard/components/Sidebar";
+import TopNav from "./features/whiteboard/components/TopNav";
+import ZoomControls from "./features/whiteboard/components/ZoomControls";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return;
+  <div className="relative w-screen h-screen overflow-hidden bg-[#0f0f14] text-gray-200">
+    {/* 1. The main drawing canvas (Base Layer) */}
+    <Canvas />
 
-export default App
+    {/* 2. UI Overlays (Floating on top) */}
+    <TopNav />
+    <Toolbar />
+    <Sidebar />
+    <ZoomControls />
+  </div>;
+};
+
+export default App;
